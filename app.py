7cb2454a9ad216c2596901c6373c36e3f1cab9af
mohammed ismail <76427625+ismailshaikh@users.noreply.github.com>
2023-08-10 23:38:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,108 @@
+import streamlit as st
+import pickle
+import numpy as np
+import time
+
+try:
+    # import the model
+    pipe = pickle.load(open('pipe.pkl','rb'))
+    df = pickle.load(open('df.pkl','rb'))
+
+    st.title("Laptop Price Predictor")
+
+
+    # brand
+    company = st.selectbox('Brand',df['Company'].unique())
+
+    # type of laptop
+    type = st.selectbox('Type',df['TypeName'].unique())
+
+    # Ram
+    ram = st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64])
+
+    # weight
+    weight = st.number_input('Weight of the Laptop')
+
+    # Touchscreen
+    touchscreen = st.selectbox('Touchscreen',['No','Yes'])
+
+    # IPS
+    ips = st.selectbox('IPS',['No','Yes'])
+
+    # screen size
+    screen_size = st.number_input('Screen Size')
+
+    # resolution
+    resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
+
+    #cpu
+    cpu = st.selectbox('CPU',df['Cpu_Brand'].unique())
+
+    #HDD
+    hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
+
+    #SSD
+    ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
+
+    #GPU
+    gpu = st.selectbox('GPU',df['Gpu_brand'].unique())
+
+    #OS
+    os = st.selectbox('OS',df['os'].unique())
+
+    # Predict Button
+    if st.button('Predict Price'):
+        # input query
+        # ppi = None
+
+        # # Touchscreen
+        # if touchscreen == 'Yes':
+        #     touchscreen=1
+        # else:
+        #     touchscreen = 0
+
+        # # ips
+        # if ips=='Yes':
+        #     ips=1
+        # else:
+        #     ips=0
+
+        # # ppi
+        # X_res = int(resolution.split('x')[0])
+        # Y_res = int(resolution.split('x')[1])
+        # ppi = (((X_res**2)+(Y_res**2))**0.5/screen_size)
+
+
+        # query = np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
+        # query = query.reshape(1,12)
+
+        # st.title(pipe.predict(query))
+
+        # query
+        ppi = None
+        if touchscreen == 'Yes':
+            touchscreen = 1
+        else:
+            touchscreen = 0
+
+        if ips == 'Yes':
+            ips = 1
+        else:
+            ips = 0
+
+        X_res = int(resolution.split('x')[0])
+        Y_res = int(resolution.split('x')[1])
+        ppi = ((X_res**2) + (Y_res**2))**0.5/screen_size
+
+        query = np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
+
+        query = query.reshape(1,12)
+        with st.spinner('Wait for it...'):
+            time.sleep(1)
+            st.success('Done!')
+        st.success("The predicted price R2_Score of this configuration is " + str(int(pipe.predict(query)[0])))
+        st.success("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))))
+        st.balloons()
+except Exception as e:
+    st.error(e)
+
